5b9d3fe0a88fa9eb7682289f719788d7989c840b
Andres Felipe Pérez G <andresfe52@gmail.com>
2021-05-18 16:57:05 -0500

    Agregar form validacion y conectarlo con form registro

---
+++ Registro/Form4.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace Registro
+{
+    partial class FormValidacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormValidacion));
+            this.lblEscanear = new System.Windows.Forms.Label();
+            this.lblForoPerfilVal = new System.Windows.Forms.Label();
+            this.grpInfoPerfil = new System.Windows.Forms.GroupBox();
+            this.txtRol = new System.Windows.Forms.TextBox();
+            this.txtPrograma = new System.Windows.Forms.TextBox();
+            this.txtCorreoInst = new System.Windows.Forms.TextBox();
+            this.txtCodEst = new System.Windows.Forms.TextBox();
+            this.txtNombreVal = new System.Windows.Forms.TextBox();
+            this.lblRol = new System.Windows.Forms.Label();
+            this.lblPrograma = new System.Windows.Forms.Label();
+            this.lblCorreoInst = new System.Windows.Forms.Label();
+            this.lblCodEstVal = new System.Windows.Forms.Label();
+            this.lblNombreVal = new System.Windows.Forms.Label();
+            this.btnVolver = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.pctFotoPerfil = new System.Windows.Forms.PictureBox();
+            this.pctEscanear = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSalirVal = new System.Windows.Forms.PictureBox();
+            this.btnValidar = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.grpInfoPerfil.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFotoPerfil)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctEscanear)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnSalirVal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblEscanear
+            // 
+            this.lblEscanear.AutoSize = true;
+            this.lblEscanear.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEscanear.Location = new System.Drawing.Point(82, 44);
+            this.lblEscanear.Name = "lblEscanear";
+            this.lblEscanear.Size = new System.Drawing.Size(166, 19);
+            this.lblEscanear.TabIndex = 1;
+            this.lblEscanear.Text = "Escanear código QR";
+            // 
+            // lblForoPerfilVal
+            // 
+            this.lblForoPerfilVal.AutoSize = true;
+            this.lblForoPerfilVal.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblForoPerfilVal.Location = new System.Drawing.Point(338, 44);
+            this.lblForoPerfilVal.Name = "lblForoPerfilVal";
+            this.lblForoPerfilVal.Size = new System.Drawing.Size(106, 19);
+            this.lblForoPerfilVal.TabIndex = 1;
+            this.lblForoPerfilVal.Text = "Foto de Perfil";
+            // 
+            // grpInfoPerfil
+            // 
+            this.grpInfoPerfil.Controls.Add(this.txtRol);
+            this.grpInfoPerfil.Controls.Add(this.txtPrograma);
+            this.grpInfoPerfil.Controls.Add(this.txtCorreoInst);
+            this.grpInfoPerfil.Controls.Add(this.txtCodEst);
+            this.grpInfoPerfil.Controls.Add(this.txtNombreVal);
+            this.grpInfoPerfil.Controls.Add(this.lblRol);
+            this.grpInfoPerfil.Controls.Add(this.lblPrograma);
+            this.grpInfoPerfil.Controls.Add(this.lblCorreoInst);
+            this.grpInfoPerfil.Controls.Add(this.lblCodEstVal);
+            this.grpInfoPerfil.Controls.Add(this.lblNombreVal);
+            this.grpInfoPerfil.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpInfoPerfil.Location = new System.Drawing.Point(12, 243);
+            this.grpInfoPerfil.Name = "grpInfoPerfil";
+            this.grpInfoPerfil.Size = new System.Drawing.Size(533, 276);
+            this.grpInfoPerfil.TabIndex = 2;
+            this.grpInfoPerfil.TabStop = false;
+            this.grpInfoPerfil.Text = "Información de Perfil";
+            // 
+            // txtRol
+            // 
+            this.txtRol.Enabled = false;
+            this.txtRol.Location = new System.Drawing.Point(217, 240);
+            this.txtRol.Name = "txtRol";
+            this.txtRol.Size = new System.Drawing.Size(296, 27);
+            this.txtRol.TabIndex = 2;
+            // 
+            // txtPrograma
+            // 
+            this.txtPrograma.Enabled = false;
+            this.txtPrograma.Location = new System.Drawing.Point(217, 192);
+            this.txtPrograma.Name = "txtPrograma";
+            this.txtPrograma.Size = new System.Drawing.Size(296, 27);
+            this.txtPrograma.TabIndex = 2;
+            // 
+            // txtCorreoInst
+            // 
+            this.txtCorreoInst.Enabled = false;
+            this.txtCorreoInst.Location = new System.Drawing.Point(217, 144);
+            this.txtCorreoInst.Name = "txtCorreoInst";
+            this.txtCorreoInst.Size = new System.Drawing.Size(296, 27);
+            this.txtCorreoInst.TabIndex = 2;
+            // 
+            // txtCodEst
+            // 
+            this.txtCodEst.Enabled = false;
+            this.txtCodEst.Location = new System.Drawing.Point(217, 96);
+            this.txtCodEst.Name = "txtCodEst";
+            this.txtCodEst.Size = new System.Drawing.Size(296, 27);
+            this.txtCodEst.TabIndex = 2;
+            // 
+            // txtNombreVal
+            // 
+            this.txtNombreVal.Enabled = false;
+            this.txtNombreVal.Location = new System.Drawing.Point(217, 47);
+            this.txtNombreVal.Name = "txtNombreVal";
+            this.txtNombreVal.Size = new System.Drawing.Size(296, 27);
+            this.txtNombreVal.TabIndex = 2;
+            // 
+            // lblRol
+            // 
+            this.lblRol.AutoSize = true;
+            this.lblRol.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRol.Location = new System.Drawing.Point(63, 243);
+            this.lblRol.Name = "lblRol";
+            this.lblRol.Size = new System.Drawing.Size(28, 16);
+            this.lblRol.TabIndex = 1;
+            this.lblRol.Text = "Rol";
+            // 
+            // lblPrograma
+            // 
+            this.lblPrograma.AutoSize = true;
+            this.lblPrograma.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrograma.Location = new System.Drawing.Point(63, 195);
+            this.lblPrograma.Name = "lblPrograma";
+            this.lblPrograma.Size = new System.Drawing.Size(72, 16);
+            this.lblPrograma.TabIndex = 1;
+            this.lblPrograma.Text = "Programa";
+            // 
+            // lblCorreoInst
+            // 
+            this.lblCorreoInst.AutoSize = true;
+            this.lblCorreoInst.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorreoInst.Location = new System.Drawing.Point(63, 147);
+            this.lblCorreoInst.Name = "lblCorreoInst";
+            this.lblCorreoInst.Size = new System.Drawing.Size(135, 16);
+            this.lblCorreoInst.TabIndex = 1;
+            this.lblCorreoInst.Text = "Correo Institucional";
+            // 
+            // lblCodEstVal
+            // 
+            this.lblCodEstVal.AutoSize = true;
+            this.lblCodEstVal.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodEstVal.Location = new System.Drawing.Point(63, 99);
+            this.lblCodEstVal.Name = "lblCodEstVal";
+            this.lblCodEstVal.Size = new System.Drawing.Size(126, 16);
+            this.lblCodEstVal.TabIndex = 1;
+            this.lblCodEstVal.Text = "Código Estudiantíl";
+            // 
+            // lblNombreVal
+            // 
+            this.lblNombreVal.AutoSize = true;
+            this.lblNombreVal.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreVal.Location = new System.Drawing.Point(63, 50);
+            this.lblNombreVal.Name = "lblNombreVal";
+            this.lblNombreVal.Size = new System.Drawing.Size(60, 16);
+            this.lblNombreVal.TabIndex = 1;
+            this.lblNombreVal.Text = "Nombre";
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.ActiveBorderThickness = 1;
+            this.btnVolver.ActiveCornerRadius = 20;
+            this.btnVolver.ActiveFillColor = System.Drawing.Color.MidnightBlue;
+            this.btnVolver.ActiveForecolor = System.Drawing.Color.White;
+            this.btnVolver.ActiveLineColor = System.Drawing.Color.MidnightBlue;
+            this.btnVolver.BackColor = System.Drawing.Color.White;
+            this.btnVolver.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnVolver.BackgroundImage")));
+            this.btnVolver.ButtonText = "Volver";
+            this.btnVolver.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVolver.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolver.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btnVolver.IdleBorderThickness = 1;
+            this.btnVolver.IdleCornerRadius = 20;
+            this.btnVolver.IdleFillColor = System.Drawing.Color.White;
+            this.btnVolver.IdleForecolor = System.Drawing.Color.MidnightBlue;
+            this.btnVolver.IdleLineColor = System.Drawing.Color.MidnightBlue;
+            this.btnVolver.Location = new System.Drawing.Point(53, 555);
+            this.btnVolver.Margin = new System.Windows.Forms.Padding(5);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(201, 41);
+            this.btnVolver.TabIndex = 3;
+            this.btnVolver.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // pctFotoPerfil
+            // 
+            this.pctFotoPerfil.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pctFotoPerfil.Location = new System.Drawing.Point(297, 82);
+            this.pctFotoPerfil.Name = "pctFotoPerfil";
+            this.pctFotoPerfil.Size = new System.Drawing.Size(182, 142);
+            this.pctFotoPerfil.TabIndex = 0;
+            this.pctFotoPerfil.TabStop = false;
+            // 
+            // pctEscanear
+            // 
+            this.pctEscanear.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pctEscanear.Location = new System.Drawing.Point(72, 82);
+            this.pctEscanear.Name = "pctEscanear";
+            this.pctEscanear.Size = new System.Drawing.Size(182, 142);
+            this.pctEscanear.TabIndex = 0;
+            this.pctEscanear.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.btnSalirVal);
+            this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(558, 28);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnSalirVal
+            // 
+            this.btnSalirVal.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnSalirVal.Image = global::Registro.Properties.Resources.emblemunreadable_93487;
+            this.btnSalirVal.Location = new System.Drawing.Point(532, 3);
+            this.btnSalirVal.Name = "btnSalirVal";
+            this.btnSalirVal.Size = new System.Drawing.Size(24, 23);
+            this.btnSalirVal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnSalirVal.TabIndex = 0;
+            this.btnSalirVal.TabStop = false;
+            this.btnSalirVal.Click += new System.EventHandler(this.btnSalirVal_Click);
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.ActiveBorderThickness = 1;
+            this.btnValidar.ActiveCornerRadius = 20;
+            this.btnValidar.ActiveFillColor = System.Drawing.Color.MidnightBlue;
+            this.btnValidar.ActiveForecolor = System.Drawing.Color.White;
+            this.btnValidar.ActiveLineColor = System.Drawing.Color.MidnightBlue;
+            this.btnValidar.BackColor = System.Drawing.Color.White;
+            this.btnValidar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnValidar.BackgroundImage")));
+            this.btnValidar.ButtonText = "Validar";
+            this.btnValidar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnValidar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidar.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btnValidar.IdleBorderThickness = 1;
+            this.btnValidar.IdleCornerRadius = 20;
+            this.btnValidar.IdleFillColor = System.Drawing.Color.White;
+            this.btnValidar.IdleForecolor = System.Drawing.Color.MidnightBlue;
+            this.btnValidar.IdleLineColor = System.Drawing.Color.MidnightBlue;
+            this.btnValidar.Location = new System.Drawing.Point(297, 555);
+            this.btnValidar.Margin = new System.Windows.Forms.Padding(5);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(201, 41);
+            this.btnValidar.TabIndex = 3;
+            this.btnValidar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormValidacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(557, 634);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.grpInfoPerfil);
+            this.Controls.Add(this.lblForoPerfilVal);
+            this.Controls.Add(this.lblEscanear);
+            this.Controls.Add(this.pctFotoPerfil);
+            this.Controls.Add(this.pctEscanear);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormValidacion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.FormValidacion_Load);
+            this.grpInfoPerfil.ResumeLayout(false);
+            this.grpInfoPerfil.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFotoPerfil)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctEscanear)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnSalirVal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctEscanear;
+        private System.Windows.Forms.PictureBox pctFotoPerfil;
+        private System.Windows.Forms.Label lblEscanear;
+        private System.Windows.Forms.Label lblForoPerfilVal;
+        private System.Windows.Forms.GroupBox grpInfoPerfil;
+        private Bunifu.Framework.UI.BunifuThinButton2 btnVolver;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox btnSalirVal;
+        private System.Windows.Forms.TextBox txtRol;
+        private System.Windows.Forms.TextBox txtPrograma;
+        private System.Windows.Forms.TextBox txtCorreoInst;
+        private System.Windows.Forms.TextBox txtCodEst;
+        private System.Windows.Forms.TextBox txtNombreVal;
+        private System.Windows.Forms.Label lblRol;
+        private System.Windows.Forms.Label lblPrograma;
+        private System.Windows.Forms.Label lblCorreoInst;
+        private System.Windows.Forms.Label lblCodEstVal;
+        private System.Windows.Forms.Label lblNombreVal;
+        private Bunifu.Framework.UI.BunifuThinButton2 btnValidar;
+    }
+}
---
+++ Registro/Form4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registro
+{
+    public partial class FormValidacion : Form
+    {
+        public FormValidacion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalirVal_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Desea cerrar la aplicación?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                Application.Exit();
+            else
+                return;
+        }
+
+        private void FormValidacion_Load(object sender, EventArgs e)
+        {
+            txtCodEst.Text = "";
+            txtNombreVal.Text = "";
+            txtPrograma.Text = "";
+            txtRol.Text = "";
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            Form2 volver = new Form2();
+            volver.Show();
+            this.Hide();
+        }
+    }
+}
