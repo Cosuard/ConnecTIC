28c0ad0498c759298c38a9d6bca5f1a5fa62883f
Cosuard <carlos.chavez@arus.com.co>
2021-05-18 20:33:04 -0500

    Correcciones

---
+++ Registro/QRModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro
+{
+    public class QRModel
+    {
+        public int Id { get; set; }
+        public string Qr { get; set; }
+    }
+}
